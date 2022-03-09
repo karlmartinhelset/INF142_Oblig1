57e949469eae9fde1e264469b882a22273f73128
Hannah.Morken <hannah.morken@uib.no>
2022-03-10 00:08:43 +0100

    welcome + champs blir nå sendt til client

--- server.py
+++ server.py
@@ -3,36 +3,40 @@ import socket
 from socket import create_server
 import threading
 from _thread import *
-import teamlocaltactics
+
+import teamlocaltactics 
 import DBHandler
+
+import pickle
+
+from rich import print
+from rich.prompt import Prompt
+from rich.table import Table
 
 
 class server:
     
     def __init__(self, host: str, port: int):
-        self.host = host
-        self.port = port
-        self.connections = []
+        self._host = host
+        self._port = port
+        self._connections = []
 
     def turn_on(self):
-        #adr = socket.gethostbyname(socket.gethostname())
-        #port = 5550
-
-        self.sock = create_server((self.host, self.port), reuse_port=True)
+        self._sock = create_server((self.host, self.port), reuse_port=True)
 
         #self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         #self.sock.bind((self.host, self.port))
         #self.sock.listen()
 
-        #self.serving = True
+        self._serving = True
+        self.accept_conn()
 
         print("Looking for connection")
-        print(socket.gethostbyname(socket.gethostname()))
 
 
     def turn_off(self):
-        self.sock.close()
-        #self.serving = False
+        self._sock.close()
+        self._serving = False
 
 """
     def threaded_client(conn):
@@ -61,19 +65,53 @@ class server:
         conn.close()
 """
 
-    #def show_champs():
-     #   champions = DBHandler.get_champs()
+    def accept_conn(self):
+        while self._serving:
+            try:
+                conn, _ = self._sock.accept()
+            except:
+                pass
+            self._connections.append(conn)
+
+            match len(self.connections):
+                case 1:
+                    self.connections[0].send("Not enough players! Waiting to start game ...".encode())
+                case 2:
+                    print("Enough players connected! Starting the game...")
+                    break
+
+        self.run_game()
+
+
+    def send_everyone(self, message):
+        for connection in self._connections:
+            connection.send(pickle.dumps(message))
+
+
+    def run_game(self):
+        welcome_message = '\n'
+          'Welcome to [bold yellow]Team Local Tactics[/bold yellow]!'
+          '\n'
+          'Each player choose a champion each time.'
+          '\n'
+        self.send_everyone(welcome_message)
+
+        champions = DBHandler.get_champs()
+        table = teamlocaltactics.print_available_champs(champions)
+        self.send_everyone(table)
+
+
+
 
 """
     while True:
-        conn, addr = sock.accept()
+        
         print("Connected to: ", addr)
         start_new_thread(threaded_client, (conn,))
 """
 
 if __name__=="__main__":
     host = socket.gethostbyname(socket.gethostname())
-    print(host)
     port = 5550
     server = server(host, port)
     server.turn_on()
