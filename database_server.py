90530a9570a1bcee791bf75e6aaff33f67ef5002
Hannah.Morken <hannah.morken@uib.no>
2022-03-16 14:41:20 +0100

    kommentarer

--- database_server.py
+++ database_server.py
@@ -9,34 +9,26 @@ from core import Champion
 
 class DBHandler:
 
-  def __init__(self, host1: str, port1: int):
-    self._host = host1
-    self._port = port1
-    self._connections = []
+  def __init__(self):
+    
+    # collection in the database created in get_database() where chamions are stored
     self.Champ_collection = self.get_database()["Champions_collection"]
+
+    # collection in the database created in get_database() where match history is stored
     self.Match_collection = self.get_database()["Match_history_collection"]
 
-  def conn_accept(self):
-
-    self.serv_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    self.serv_sock.bind((self._host, self._port))
-    self.serv_sock.listen()
-   
-    while True:
-      conn, _ = self.serv_sock.accept()
-      self._connections.append(conn)
 
   def get_database(self):
 
-    # Get you password from .env file
+    # Get the password from .env file
     password = os.getenv("PASSWORD")
 
     clusterName = "Oblig1142Cluster"
 
-    # Connect to you cluster
+    # Connect to the cluster
     client = MongoClient("mongodb+srv://hannahmorken:" + password + "@Oblig1142Cluster.clrn2.mongodb.net/TeamNetworkTactics", tlsCAFile=certifi.where())
 
-    # Create a new database in your cluster
+    # Create a new database in the cluster
     database = client["TeamNetworkTactics"]
     return database
 
@@ -47,27 +39,25 @@ class DBHandler:
 
   def get_champs(self):
     all_champions = {} 
+
+    # loop through each champion in the collection
     for x in self.Champ_collection.find():
+        # get champion with name, rockProbability, paperProbability and scissorsProbability
         champion = Champion(x["Name"], float(x["rockProbability"]), float(x["paperProbability"]), float(x["scissorsProbability"]))
+        
+        # add champion to new dict
         all_champions[x["Name"]] = champion
+
     return all_champions
 
-
-  # Matches
 
   def add_new_match(self, match):
     self.Match_collection.insert_one(match)
 
 
   def get_match_history(self, nMatches):
-    matchList = self.Match_collection.find({}).limit(nMatches)
-    return matchList
+    return self.Match_collection.find({}).limit(nMatches)
 
-def db_main():
-  host = 'localhost'
-  port = 7020
-  serv = DBHandler(host, port)
-  serv.conn_accept()
 
 if __name__ == "__main__":
-  db_main()
+  serv = DBHandler()
